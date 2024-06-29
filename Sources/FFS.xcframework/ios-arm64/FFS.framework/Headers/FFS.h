83f51aa78ad36e3f6b0ffaec21b32dd9f29d8004
Stavros Pachoundakis <stavroschios@icloud.com>
2024-06-29 00:26:29 -0400

    version 0.0.2

---
+++ Sources/FFS.xcframework/ios-arm64/FFS.framework/Headers/FFS.h
@@ -0,0 +1,18 @@
+//
+//  FFS.h
+//  FFS
+//
+//  Created by Stavros Pachoundakis on 2024-06-29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FFS.
+FOUNDATION_EXPORT double FFSVersionNumber;
+
+//! Project version string for FFS.
+FOUNDATION_EXPORT const unsigned char FFSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFS/PublicHeader.h>
+
+
--- Sources/FFS/FFS.swift
+++
@@ -1,9 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-import Foundation
-
-public struct SDK {
-    public static func run() {
-        print("Works")
-    }
-}
